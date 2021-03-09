416c25d8f46ab4ca64209dfddcc86e7905e43801
RMorjane <rhellabmorjane@gmail.com>
2021-03-09 17:10:21 +0100

    création de la table person avec un champ id

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ def connect():
         cnt_string = "host={0} user={1} dbname={2} password={3}".format(host,user,dbname,password)
         connection = psycopg2.connect(cnt_string)
         print("Connexion réussie : " + str(connection))
+        return connection
     except (Exception, psycopg2.Error) as error:
         print("Impossible de se connecter au serveur postgres : " + str(error))
+        return None
 
-connect()
+def create_table():
+    cnt = connect()
+    if cnt:
+        try:
+            cur = cnt.cursor()
+            sql_create_table = "CREATE TABLE person(id INT NOT NULL PRIMARY KEY)"
+            cur.execute(sql_create_table)
+            print("Création de la table réussie")
+        except (Exception, psycopg2.Error) as error:
+            print("Impossible de créer la table dans la base postgres : " + str(error))
+
+create_table()
